5182b02d635b258c99886b60a69a43b98acfffdf
Kushal Chordiya <chordiyakushal@gmail.com>
2024-03-13 07:04:28 +0530

    update about

--- components/about.tsx
+++ components/about.tsx
@@ -11,7 +11,7 @@ export default function About() {
   return (
     <motion.section
       ref={ref}
-      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      className="mb-28 max-w-[45rem] leading-8 sm:mb-40 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
@@ -19,30 +19,25 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
+        After graduating with a bachelor's degree in{" "}
+        <span className="font-medium">Computer Science</span>, I joined as a Software engineer in Trilogy Inovations.{" "}
+      </p>
+      <p>
+        <span className="italic">My favorite part of programming</span> has always been the
         problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+        feeling of finally figuring out a solution to a problem. 
+        During my time at TI, I gained exposure to many practical skills, both technical, like my expertise in AWS and non-technical, like truly understanding your customers, to deliver maximum value to them. 
+        I will soon be joining Indeed as a SWE-2{" "}
+      </p>
+      <p>
+      I specialize in backend engineering, especially building on top of cloud services, however I'm also skilled with building front-ends with React.
+      I'm also experienced with building ML pipelines and AI-powered applications.
+      I am always looking to learn new technologies.
       </p>
 
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        badminton, watching movies, and reading books on fiction and philosophy.
       </p>
     </motion.section>
   );
